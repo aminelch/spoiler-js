5946e6c1f9c294db6bb59b2653275f7e2f25f2aa
Amine L'ch <aminelch95@gmail.com>
2021-01-08 23:36:01 +0100

    waiting until DOM loaded and encapsulate spoiler js code

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,36 +1,39 @@
-(function () {
+window.onload = () => {
 
-    const btnTextContent = "afficher le spoiler"
-    // on selectionne les span qui ont la classe .spoiler
-    let spans = document.querySelectorAll(".spoiler")
+    (function () {
 
-    // on parcours la liste des elements
-    for (let index = 0; index < spans.length; index++) {
-        const element = spans[index];
-        createSpoilerButton(element)
-    }
+        const btnTextContent = "afficher le spoiler"
+        // on selectionne les span qui ont la classe .spoiler
+        let spans = document.querySelectorAll(".spoiler")
 
-    /**
-     * permet de créer un button pour un spoiler
-     * @param {HTMLElement} element
-     */
-    function createSpoilerButton(element) {
-             //on crée le button
-             let button = document.createElement("button")
-             //on modifie le text du button
-             button.textContent = btnTextContent
-             // button.dataset.spoilerContent=span.innerHTML
-             // injecte le button après le span
-             element.after(button)
+        // on parcours la liste des elements
+        for (let index = 0; index < spans.length; index++) {
+            const element = spans[index];
+            createSpoilerButton(element)
+        }
 
-             // on ecoute sur le click de chaque button
-             button.addEventListener("click", () => {
-                 // on ajoute la classe visible au span
-                 element.classList.add("visible")
-                 // on supprimer le button
-                 button.parentNode.removeChild(button)
-                 console.log('btn clicked')
-             })
-   }
+        /**
+         * permet de créer un button pour un spoiler
+         * @param {HTMLElement} element
+         */
+        function createSpoilerButton(element) {
+            //on crée le button
+            let button = document.createElement("button")
+            //on modifie le text du button
+            button.textContent = btnTextContent
+            // button.dataset.spoilerContent=span.innerHTML
+            // injecte le button après le span
+            element.after(button)
 
-})()
+            // on ecoute sur le click de chaque button
+            button.addEventListener("click", () => {
+                // on ajoute la classe visible au span
+                element.classList.add("visible")
+                // on supprimer le button
+                button.parentNode.removeChild(button)
+                console.log('btn clicked')
+            })
+        }
+
+    })()
+}

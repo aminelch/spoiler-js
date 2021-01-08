b8d8066fba1530bc695ba60a36d382ea01abd5f9
Amine L'ch <aminelch95@gmail.com>
2021-01-08 23:25:01 +0100

    refactor spoiler js code

--- js/spoiler.js
+++ js/spoiler.js
@@ -7,25 +7,30 @@
     // on parcours la liste des elements
     for (let index = 0; index < spans.length; index++) {
         const element = spans[index];
-
-        //on crée le button
-        let button = document.createElement("button")
-        //on modifie le text du button
-        button.textContent = btnTextContent
-        // button.dataset.spoilerContent=span.innerHTML
-        // injecte le button après le span
-        element.after(button)
-
-        // on ecoute sur le click de chaque button
-        button.addEventListener("click", () => {
-            // on ajoute la classe visible au span
-            element.classList.add("visible")
-            // on supprimer le button
-            button.parentNode.removeChild(button)
-            console.log('btn clicked')
-        })
-
+        createSpoilerButton(element)
     }
 
+    /**
+     * permet de créer un button pour un spoiler
+     * @param {HTMLElement} element
+     */
+    function createSpoilerButton(element) {
+             //on crée le button
+             let button = document.createElement("button")
+             //on modifie le text du button
+             button.textContent = btnTextContent
+             // button.dataset.spoilerContent=span.innerHTML
+             // injecte le button après le span
+             element.after(button)
+
+             // on ecoute sur le click de chaque button
+             button.addEventListener("click", () => {
+                 // on ajoute la classe visible au span
+                 element.classList.add("visible")
+                 // on supprimer le button
+                 button.parentNode.removeChild(button)
+                 console.log('btn clicked')
+             })
+   }
 
 })()
